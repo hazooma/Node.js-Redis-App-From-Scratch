8073dca6fb3417474f04616c03181c224613fdc8
Xsozm <hazem29778865@gmail.com>
2019-08-01 01:14:55 +0200

    AddViews

--- app.js
+++ app.js
@@ -5,11 +5,36 @@ import bodybarser from "body-parser";
 import methodoverride from "method-override";
 import redis from "redis";
 
-// chose port
-const PORT = 6666;
+// Create Redis Client
+let client = redis.createClient();
+client.on("connect", () => {
+  console.log("REdis Connected !");
+});
 
-// init app
+// Chose port
+const PORT = 4000;
+
+// Init app
 const app = express();
 
+// View Engine
 app.engine("handlebars", expresshbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Body Parser
+app.use(bodybarser.json());
+app.use(bodybarser.urlencoded({ extended: false }));
+
+app.use(methodoverride("_method"));
+
+app.get("/", (req, res, next) => {
+  res.render("searchusers");
+});
+
+app.post("/users/serach", (req, res, next) => {
+  let id = req.body.id;
+});
+app.listen(PORT, error => {
+  if (error) console.log(error);
+  else console.log("conncted");
+});
